a63eafc09e1a58f7712195b940be56e2c8be7de3
Alla Dolzhenko <kweyjiba12@gmail.com>
2023-11-06 10:45:51 +0100

    Star rating

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="#333" className="test" defaultRating={2} />
+  </React.StrictMode>
+);
